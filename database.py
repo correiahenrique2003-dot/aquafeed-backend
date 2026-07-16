98fad1d768347b91b2eba3a7d6ed118523a945df
Henrique Correia Alves da Silva <correia.henrique2003@gmail.com>
2026-07-16 09:12:35 -0300

    Configura conexão com Supabase usando PostgreSQL

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./aquafeed.db"
+# Carrega as variáveis do .env
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
