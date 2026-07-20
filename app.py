a2315a8189a31833df700818dcefb0eefa413cdd
Henrique Correia Alves da Silva <correia.henrique2003@gmail.com>
2026-07-20 08:11:17 -0300

    Configura CORS para integração com frontend

--- app.py
+++ app.py
@@ -13,18 +13,6 @@ app = FastAPI(
     title="AquaFeed API"
 )
 
-origins = [
-    "http://localhost:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
