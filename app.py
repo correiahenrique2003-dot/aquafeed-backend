4a703869bcad36ebb8d2cb8b0ee0a985f90c20df
Henrique Correia Alves da Silva <correia.henrique2003@gmail.com>
2026-07-19 23:22:51 -0300

    Adicionando CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base, SessionLocal
 
 import crud
@@ -12,6 +13,27 @@ app = FastAPI(
     title="AquaFeed API"
 )
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = SessionLocal()
